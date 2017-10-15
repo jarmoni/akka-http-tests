292654c59d1ac34d61fa4ff03cf9cbe407760302
ms <ms@example.com>
2017-10-15 21:12:24 +0200

    added swagger

--- src/main/scala/org.jarmoni/akka_http_tests/WebServer.scala
+++ src/main/scala/org.jarmoni/akka_http_tests/WebServer.scala
@@ -1,15 +1,41 @@
 package org.jarmoni.akka_http_tests
 
+import java.io.InputStream
+import java.security.{KeyStore, SecureRandom}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
 import akka.stream.ActorMaterializer
+import com.typesafe.scalalogging.LazyLogging
 import com.typesafe.sslconfig.akka.AkkaSSLConfig
 
 import scala.io.StdIn
 
-object WebServer {
+object WebServer extends LazyLogging {
+
+  def createSslContext(): HttpsConnectionContext = {
+    val password: Array[Char] = "s3cr3t".toCharArray // do not store passwords in code, read them from somewhere safe!
+
+    val ks: KeyStore = KeyStore.getInstance("JKS")
+    val keystore: InputStream = getClass.getClassLoader.getResourceAsStream("example.com.jks")
+
+    require(keystore != null, "Keystore required!")
+    ks.load(keystore, password)
+
+    val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(ks, password)
+
+    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+    tmf.init(ks)
+
+    val sslContext: SSLContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom())
+    ConnectionContext.https(sslContext)
+  }
+
   def main(args: Array[String]) {
 
     implicit val system = ActorSystem("my-system")
@@ -18,6 +44,11 @@ object WebServer {
     implicit val executionContext = system.dispatcher
     val sslConfig = AkkaSSLConfig()
 
+    val https = createSslContext()
+    Http().setDefaultServerHttpContext(https)
+
+    logger.info("Just a test")
+
     val route =
       path("hello") {
         get {
@@ -25,7 +56,13 @@ object WebServer {
         }
       }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val (host, port) = ("localhost", 8443)
+
+    val bindingFuture = Http().bindAndHandle(route, host, port, connectionContext = https)
+
+    bindingFuture.failed.foreach { ex =>
+      logger.error(s"Failed to bind to ${host}:${port}!", ex)
+    }
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
